e4e2f00c428cbc6ee9ddf4cc32c2d013248f1149
Jhonatan Konopp <jhonnkonopp@gmail.com>
2023-10-12 01:41:23 -0300

    feat: created token colors for tailwindcss

---
+++ src/tokens/colors.ts
@@ -0,0 +1,12 @@
+export const colors = {
+	alert: '#BF0E0E',
+	black: '#000000',
+	grey: '#BFBFBF',
+	white: '#ffffff',
+	teal: {
+		400: '#4E9590'
+	},
+	peach: {
+		200: '#F2CAB8'
+	}
+} as const;
