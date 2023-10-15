6a45a904ad694668dbe281a93235e0f9e099c657
Jhonatan Konopp <jhonnkonopp@gmail.com>
2023-10-15 15:32:47 -0300

    feat: added disabled prop Button

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -4,12 +4,14 @@ import { ReactElement, ReactNode } from 'react';
 interface ButtonProperties {
 	children: ReactNode;
 	onClick: () => void;
+	disabled?: boolean;
 	className?: string;
 }
 
 export default function Button({
 	children,
 	className,
+	disabled,
 	onClick
 }: ButtonProperties): ReactElement {
 	return (
@@ -21,8 +23,10 @@ export default function Button({
 				'text-base font-medium text-black',
 				'py-[0.375rem]',
 				'focus:outline-none',
+				'disabled:cursor-not-allowed  disabled:border-0 disabled:opacity-75',
 				className
 			)}
+			disabled={disabled}
 		>
 			{children}
 		</button>
