4730b5b947187d093a4342e2f9cf2e79460d4891
Jhonatan Konopp <jhonnkonopp@gmail.com>
2023-10-15 14:50:46 -0300

    fix: unique key errors

--- src/components/JsonViewer/hooks/useRenderItem.tsx
+++ src/components/JsonViewer/hooks/useRenderItem.tsx
@@ -1,3 +1,4 @@
+import { nanoid } from 'nanoid';
 import { ReactElement } from 'react';
 import ArrayValue from '../components/ArrayValue';
 import ObjectValue from '../components/ObjectValue';
@@ -17,31 +18,49 @@ export default function useRenderItem(): UseRenderItemResponse {
 			case 'number':
 				if (property) {
 					return (
-						<div className='flex items-start justify-start gap-x-1'>
+						<div
+							className='flex items-start justify-start gap-x-1'
+							key={`${String(item)}-${nanoid()}`}
+						>
 							<Property property={property} />{' '}
 							<Value value={item} />
 						</div>
 					);
 				} else {
-					return <Value value={item} />;
+					return (
+						<Value
+							value={item}
+							key={`${String(item)}-${nanoid()}`}
+						/>
+					);
 				}
 			case 'object': {
 				// Array, object or null
 				if (item === null && property) {
 					return (
-						<div className='flex items-start justify-start gap-x-1'>
+						<div
+							className='flex items-start justify-start gap-x-1'
+							key={`${String(item)}-${nanoid()}`}
+						>
 							<Property property={property} />{' '}
 							<Value value={item} />
 						</div>
 					);
 				}
 				if (Array.isArray(item) && property) {
-					return <ArrayValue array={item} property={property} />;
+					return (
+						<ArrayValue
+							array={item}
+							property={property}
+							key={`${String(item)}-${nanoid()}`}
+						/>
+					);
 				}
 				return (
 					<ObjectValue
 						object={item as Record<string, unknown>}
 						property={property}
+						key={`${String(item)}-${nanoid()}`}
 					/>
 				);
 			}
